b2fc0e511619e140e4bd6c9e42e9d4c4e12e1df6
student <farodriguezcabral@miners.utep.edu>
2017-09-27 14:50:54 -0600

    Fixed indentation and comments

--- shell/shell.c
+++ shell/shell.c
@@ -9,43 +9,31 @@
 int main(int arcg, char **argv, char** envp){
   
   while(1){
-    char *input = (char*)malloc(100);
+    char *input = (char*)malloc(100);//store user input
     write(1,"$",1);
-    //read user input
-    read(0,input,100);
-    //tokenize the command entered by the user using mytoc()
-    char **command  = mytoc(input,' ');
-    int child = saferFork();
+    read(0,input,100); //read user input
+    char **command  = mytoc(input,' '); //tokenize the command entered by the user using mytoc()
+    int child = saferFork(); //Create a child process
     free(input);
-    
-    //If process is a child, run the command
-    if(child == 0){
-      int retVal = 0;
-      //store the PATH variable when found
-      char **path;
-      //iterate through envp to look for PATH
-       for(int i = 0; envp[i] != 0; i++){
-	 //tokenize envp[i]
-	path = mytoc(envp[i], '=');
-        //if PATH is found, execute the command
-	if(!strcmp(path[0],"PATH")){
-	  retVal = execve(path[1],command,envp);
-	  fprintf(stderr, "%s: exect returned %d\n", command[0],retVal);
-	  
-	}
-	//free path in order to allow a new vector to be stored in case PATH is not found
-	for(;*path != '\0';path++){
-	  free(*path);
-	}
-       
-       }	 
-	  
-    }
-      
+   
+    if(child == 0) //If process is a child,try executing the command
+      {
+	int retVal = 0; //get return from execve to check for errors
+	char **path; //store the PATH variable when found
+	int i = 0;
+	for(;envp[i] != 0; i++) //iterate through envp to look for PATH
+	  { 
+	    path = mytoc(envp[i], '='); //tokenize envp[i]
+	    if(!strcmp(path[0],"PATH")) //if PATH is found, execute the command
+	      break;
+	    
+	    for(;*path != '\0';path++)//free path in order to allow a new vector to be stored in case PATH is not found
+	      free(*path);  
+	  }
+	
+	retVal = execve(path[1],command,envp);
+	fprintf(stderr, "%s: exect returned %d\n", command[0],retVal);
+      }
   }
-  
   return 0;
-
 }
-  
-
