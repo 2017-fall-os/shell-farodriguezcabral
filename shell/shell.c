2a295f74fa06a7ab76fe976b8d7b3cd835553285
student <farodriguezcabral@miners.utep.edu>
2017-09-27 10:08:54 -0600

    shell retrieving a double free error

--- shell/shell.c
+++ shell/shell.c
@@ -18,20 +18,22 @@ int main(int arcg, char **argv, char** envp){
     int child = saferFork();
     free(input);
     
-    
+    //If process is a child, run the command
     if(child == 0){
       int retVal = 0;
-     
+      //store the PATH variable when found
       char **path;
+      //iterate through envp to look for PATH
        for(int i = 0; envp[i] != 0; i++){
-      
+	 //tokenize envp[i]
 	path = mytoc(envp[i], '=');
-        
+        //if PATH is found, execute the command
 	if(!strcmp(path[0],"PATH")){
 	  retVal = execve(path[1],command,envp);
 	  fprintf(stderr, "%s: exect returned %d\n", command[0],retVal);
 	  
 	}
+	//free path in order to allow a new vector to be stored in case PATH is not found
 	for(;*path != '\0';path++){
 	  free(*path);
 	}
