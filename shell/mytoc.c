f61d719925ffd46d20c4e4d64d374493042c3870
student <farodriguezcabral@miners.utep.edu>
2017-10-01 17:47:10 -0600

    changes made to tokenizer, still not working

--- shell/mytoc.c
+++ shell/mytoc.c
@@ -2,65 +2,92 @@
 #include <stdio.h>
 #include <stdlib.h> //for malloc and calloc
 
+int totalTokens(char* str,char delim);
+
 char **mytoc(char *str, char delim){
   char *copyStr;//helper string to keep the current token to be stored
   char **tokenVec; //vector containing the tokens to be returned
-  int numOfTokens = 0; //num of tokens contained in the input string
+  
   int counter = 0; //counter that keeps track of the current position in the string input
-  //while loop will stop once the null character is reached
-  while(str[counter] != '\0'){
-    //check wheter the delim character has been found or the last token has been reached
-    if(str[counter] == delim || str[counter+1] == '\0')
-      numOfTokens++;
-
-    counter++;
-  }
-  //allocate memory for tokenVect according to the number of tokens in string
-  tokenVec = (char **)calloc(numOfTokens+1, sizeof(char*));
-  counter = 0; //make counter equal to zero so it can be reused to go again throuh the string
+  int numOfTokens = totalTokens(str,delim);
+  
+  printf("numoftokens: %d",numOfTokens);
+  
+  tokenVec = (char **)calloc(numOfTokens+1, sizeof(char*));//allocate memory for tokens
   
   int charCounter = 0; //to store the length of each token
   int tokenPosition = 0; //to determine which token position we are at
-      
-  while(str[counter] != '\0'){
-    //check whether the delim character has been reached or the end of the string has been reached
-    if(str[counter] == delim || str[counter+1] == '\0'){
-      //if statement avoids missing out the last character of the string before the end of line
-      if(str[counter+1] == '\0'){
+  char * begin = str; //to contain beginning of str 
+  
+  for(;*begin != '\0'; begin++)//allocate enough memory for each character
+    {
+      if (*begin == delim)
+	{
+	  tokenVec[tokenPosition] = (char*)malloc(charCounter+1);
+	  tokenPosition++;
+	  charCounter = 0;
+	}
+      else
 	charCounter++;
-	counter++;
-       
-      }      
-      //allocate memory for the corresponding token position
-      tokenVec[tokenPosition] = (char *)malloc(charCounter+1);//allocate memory for the token at
-      //allocate memory for the copyStr which will hold the current token to be stored in the array
-      copyStr = (char *)malloc(charCounter+1);
-      //add the null character at the end of the copyStr
-      copyStr[charCounter+1] = '\0';
-      //counter to keep track of the current position on the token to copied
-      int copyCounter = 0;
-      while(charCounter > 0){
-	//copy character by carachter from the input string to the copyStr
-	copyStr[copyCounter] = str[counter-charCounter];
-	
-	charCounter--;
-	copyCounter++;
-      }
-
-      //store the copied string into the array
-      tokenVec[tokenPosition] = copyStr; 
-      tokenPosition++;
-     
     }
-    else{
-      charCounter ++;
-
+  
+  tokenPosition = 0;
+  charCounter = 0;
+  for(;*str != '\0'; str++)//copy string to vector
+    {
+      if(*str != delim)
+	{
+	  tokenVec[tokenPosition][charCounter] = *str;
+	  charCounter++;
+	}
+      else
+	{
+	  tokenVec[tokenPosition][charCounter+1] = '\0';
+	  charCounter = 0;
+	  tokenPosition++;
+	}
     }
-    counter++;  
+  
+  tokenVec[tokenPosition+1]; 
 
-  }
   //add the null character at the end of the token
-  tokenVec[tokenPosition+1] = '\0';
+  tokenVec[numOfTokens+1] = '\0';
   //return the array of strings
+  
   return tokenVec;
 }
+
+
+int totalTokens(char* str,char delim)
+{
+  if (str == NULL)
+    return 0;
+  
+  int numOfTokens = 0; //num of tokens contained in the input string
+  for(;*str != '\0';str++) //for-loop will stop once the null character is reached
+    { 
+      if(*str == delim)//check wheter the delim character has been found or the last token has been reached
+	numOfTokens++;
+    }
+  return numOfTokens+1;
+}
+
+//char *storeToken(char token[])
+//{
+// char *storedToken = NULL;
+//  int lenght = 0;
+  
+//  while(token[lenght] != '\0')
+//   {
+//     lenght++;
+//   }
+// storedToken = (char*)calloc(lenght+1,sizeof(char));
+// int i = 0;
+// while(token[i] != '\0')
+//   {
+//    *(storedToken+i) = token[i];
+//    i++;
+//   }
+// *(word + (i+1)) = '\0';
+// return storedToken;
+//}
