6f26aecb1fa9f9e8f3a71acdecaca9dc0680c39f
student <farodriguezcabral@miners.utep.edu>
2017-10-08 19:00:43 -0600

    added shell

--- shell/shell.c
+++ shell/shell.c
@@ -17,57 +17,28 @@ int main(int argc, char **argv, char** envp){
     
     int status;
     pid_t pid, wpid ; //Create a child process
-
-  
-    //while(child = )
-      // free(input);
+    pid = fork();
    
-    if((pid = fork()) == 0) //If process is a child,try executing the command
+    if(pid == 0) //If process is a child,try executing the command
       {
 	int retVal = 0; //get return from execve to check for errors
-	//char **path; //store the PATH variable when found
-	
-	//int i;
-	
-	//for(i = 0;envp[i] != (char*)0; i++) //iterate through envp to look for PATH
-	
-	//for(;*path != '\0';path++)//free path in order to allow a new vector to be stored in case PATH is not found
-	//	printf("envp[%d] = \"\n", i);
-	char **command  = mytoc(input,' '); //tokenize the command entered by the user using mytoc()
-	char **path = mytoc(envp[26],'=');
-	//	for(;*path!='\0';path++)
-	//printf("path=\"%s\n",*path);
-	      
-	
-	    // printf("envp[%d] = \"%s\"\n", i, envp[i]);
-	    //char ** path = mytoc(envp[i], '='); //tokenize envp[i]
-	    //printf("path[%d] = \"%s\"\n", i, path[i]);
-	    
-	    //if(!strcmp(path[0],"PATH")) //if PATH is found, execute the command
-		//{
-	   
-		// break;
-	    //}
         
-	    //free(path);
-	int lenght = numOfTokens(input);
-	command[lenght+1] =  '\0'; 
-	char *temp1 = strcat(command[0],path[1]);
-	char * temp = strcat('\0',temp1);
-	//	printf("path[] = \"%s\"\n",path[1]);
-	retVal = execve(path[1],command,envp);
+	char **command  = mytoc(input,' '); //tokenize the command entered by the user using mytoc()
+	int lenght = 0;
+	for(;*command; command++){
+	  lenght++;
+	}
+	char **path = mytoc(envp[26],'=');
+	char * temp = strcat('\0',path[1]);
+        command[lenght+1] = NULL;
+	//printf("path[] = \"%s\"\n",path[1]);
+	retVal = execve(temp,command,envp);
 	fprintf(stderr, "%s: exect returned %d\n", command[0],retVal);
-	//exec(path[1],command,envp);
-	//	for(;*path != '\0';path++)//free path in order to allow a new vector to be stored in case PATH is not found
-		  //free(*path);
 
       }
     wait(NULL);
-    //else
-    //while(wait(&status) > 0)
-    //	write(1,"7",1);
-     
-      
+    else
+      while(wait(&status) > 0)
   }
   return 0;
 }
